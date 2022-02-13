f4fa98d50f8c61fa7f203335568f70e0626bd3b7
sharonkorir <sharon.korir@student.moringaschool.com>
2022-02-13 12:04:47 +0300

    import httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './landing/landing.component';
 import { MatIconModule } from '@angular/material/icon';
-import {MatGridListModule} from '@angular/material/grid-list';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,7 +18,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
-    MatGridListModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
