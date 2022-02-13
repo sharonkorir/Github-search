7b7772495fe7cfcb87ced9d1ad676f37a2efa8cd
sharonkorir <sharon.korir@student.moringaschool.com>
2022-02-13 12:30:15 +0300

    add access token to environment

--- src/app/git-service/git.service.ts
+++ src/app/git-service/git.service.ts
@@ -1,9 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  accessToken: "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"
 };
 
 /*
