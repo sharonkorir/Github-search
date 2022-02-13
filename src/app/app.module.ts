c1c4eb9bb0ef2e062f4dcf7fd9a1193f06e9b194
sharonkorir <sharon.korir@student.moringaschool.com>
2022-02-13 10:06:51 +0300

    create landing and search form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingComponent } from './landing/landing.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
